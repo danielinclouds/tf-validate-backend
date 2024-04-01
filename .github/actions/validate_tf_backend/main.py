cd0c27a5aa88dcfb3791323ca2605b5ca5ceaff7
Daniel Kozlowski <dkozlowski.external@epo.org>
2024-04-02 00:23:10 +0100

    play

--- .github/actions/validate_tf_backend/main.py
+++ .github/actions/validate_tf_backend/main.py
@@ -11,25 +11,25 @@ path = os.environ.get("FILE_PATH")
 # path = "./project1/app1/main.tf"
 
 
-print("=============================")
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 print(state_bucket)
 print(path)
-print("=============================")
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-with open(path, 'r') as f:
-    dict = hcl2.load(f)
+# with open(path, 'r') as f:
+#     dict = hcl2.load(f)
 
-    # Normalize the path
-    base = os.path.dirname(path)
-    base = base.removeprefix('./')
+#     # Normalize the path
+#     base = os.path.dirname(path)
+#     base = base.removeprefix('./')
 
-    prefix = dict['terraform'][0]['backend'][0]['gcs']['prefix']
-    prefix = prefix.removeprefix('/')
-    prefix = prefix.removesuffix('/')
+#     prefix = dict['terraform'][0]['backend'][0]['gcs']['prefix']
+#     prefix = prefix.removeprefix('/')
+#     prefix = prefix.removesuffix('/')
 
-    if base == prefix:
-        print("Prefix is correct")
+#     if base == prefix:
+#         print("Prefix is correct")
 
-    if state_bucket == dict['terraform'][0]['backend'][0]['gcs']['bucket']:
-        print("Bucket name is correct")
+#     if state_bucket == dict['terraform'][0]['backend'][0]['gcs']['bucket']:
+#         print("Bucket name is correct")
 
